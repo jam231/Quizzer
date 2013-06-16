5a34fff9226dbb2378bc387125105dace8ad0536
kaiks <karol.lewcio@gmail.com>
2013-06-16 22:48:14 +0200

    Początki

---
+++ quiz_management/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizManagement::Application.config.secret_token = '0da65830850fe9a6f240b117e67abe41686fb1d1fdce3da1b95239c09df116e5289e5521fe821e5a7cc2354bbdfb65c027f6031c082fbdafdcad0a9f72d6bad7'
---
+++ quiz_management/db/schema.rb
@@ -0,0 +1,106 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 0) do
+
+  create_table "dostep_grupa", :id => false, :force => true do |t|
+    t.integer "id_grupy",                                                 :null => false
+    t.integer "id_uz",                                                    :null => false
+    t.string  "prawa_dost", :limit => 16, :default => "1100000000000000", :null => false
+  end
+
+  create_table "dyskusja", :id => false, :force => true do |t|
+    t.integer  "id_quizu",                     :null => false
+    t.integer  "id_uz",                        :null => false
+    t.string   "tresc",         :limit => nil, :null => false
+    t.datetime "data_wyslania",                :null => false
+  end
+
+  create_table "grupa_quizowa", :primary_key => "id_grupy", :force => true do |t|
+    t.integer "id_wlasciciela",               :null => false
+    t.string  "nazwa",          :limit => 60, :null => false
+    t.boolean "na_zaproszenie",               :null => false
+    t.string  "haslo",          :limit => 30
+  end
+
+  create_table "kategoria", :primary_key => "id_kategorii", :force => true do |t|
+    t.string "nazwa", :limit => 60, :null => false
+  end
+
+  add_index "kategoria", ["nazwa"], :name => "kategoria_nazwa_key", :unique => true
+
+  create_table "odpowiedz_uzytkownika", :id => false, :force => true do |t|
+    t.integer  "id_uz",                        :null => false
+    t.string   "tresc_odp",     :limit => nil, :null => false
+    t.integer  "id_pyt",                       :null => false
+    t.datetime "data_wyslania",                :null => false
+    t.boolean  "zaznaczona",                   :null => false
+  end
+
+  create_table "odpowiedz_wzorcowa", :id => false, :force => true do |t|
+    t.integer  "id_pyt",                            :null => false
+    t.string   "tresc_odp",          :limit => nil, :null => false
+    t.integer  "poziom_poprawnosci",                :null => false
+    t.string   "komentarz",          :limit => nil
+    t.datetime "ost_modyfikacja"
+  end
+
+  create_table "podkategoria", :id => false, :force => true do |t|
+    t.integer "id_nadkategorii", :null => false
+    t.integer "id_podkategorii", :null => false
+  end
+
+  add_index "podkategoria", ["id_podkategorii"], :name => "podkategoria_id_podkategorii_key", :unique => true
+
+  create_table "pytanie", :primary_key => "id_pyt", :force => true do |t|
+    t.string  "tresc",        :limit => nil,                  :null => false
+    t.integer "id_typu",                                      :null => false
+    t.integer "id_autora",                                    :null => false
+    t.float   "pkt",                         :default => 1.0, :null => false
+    t.integer "id_quizu",                                     :null => false
+    t.integer "id_kategorii",                                 :null => false
+  end
+
+  create_table "quiz", :primary_key => "id_quizu", :force => true do |t|
+    t.integer  "id_wlasciciela",                                :null => false
+    t.string   "nazwa",           :limit => 60,                 :null => false
+    t.integer  "id_grupy",                       :default => 1, :null => false
+    t.integer  "limit_podejsc"
+    t.string   "limit_czasowy",   :limit => nil
+    t.datetime "data_utworzenia",                               :null => false
+  end
+
+  create_table "ranking", :id => false, :force => true do |t|
+    t.integer "id_uz",                     :null => false
+    t.integer "id_grupy",                  :null => false
+    t.float   "pkt",      :default => 0.0, :null => false
+  end
+
+  create_table "typ", :primary_key => "id_typu", :force => true do |t|
+    t.string  "nazwa",      :limit => 60, :null => false
+    t.integer "liczba_odp",               :null => false
+  end
+
+  create_table "uzytkownik", :primary_key => "id_uz", :force => true do |t|
+    t.string "login",    :limit => 15,                           :null => false
+    t.string "haslo",    :limit => 30,                           :null => false
+    t.string "nazwa_uz", :limit => 30,                           :null => false
+    t.string "email",    :limit => 60,                           :null => false
+    t.string "ranga",    :limit => 30, :default => "uľytkownik", :null => false
+  end
+
+  add_index "uzytkownik", ["email"], :name => "uzytkownik_email_key", :unique => true
+  add_index "uzytkownik", ["login"], :name => "uzytkownik_login_key", :unique => true
+  add_index "uzytkownik", ["nazwa_uz"], :name => "uzytkownik_nazwa_uz_key", :unique => true
+
+end
