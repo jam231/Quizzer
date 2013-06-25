ed23a41463b11c41989bf64fe3d18cb3a33a78af
jam231 <mateusz.jany@gmail.com>
2013-06-25 02:50:32 +0200

    Nie dump, ale 3 uzytkownikow z roznymi rangami.

---
+++ SQL/przykladowi_uzytkownicy.sql
@@ -0,0 +1,3 @@
+INSERT INTO Uzytkownik(login,haslo,nazwa_uz,email,ranga) VALUES('administrator', 'administrator', 'GOD', 'basement@home.com', 'DEUS'); 
+INSERT INTO Uzytkownik(login,haslo,nazwa_uz,email,ranga) VALUES('Moderator', 'Moderator123', 'Moderator1', 'xxx@xxx.com', 'DEMIGOD'); 
+INSERT INTO Uzytkownik(login,haslo,nazwa_uz,email,ranga) VALUES('Adam', 'waz', 'Ewa', 'dragqueen@home.com', 'uzytkownik');
