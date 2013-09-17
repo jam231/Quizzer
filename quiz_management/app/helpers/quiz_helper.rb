499e7401b7bfd9d7373983fb49522ff6d67e4a25
jam231 <mateusz.jany@gmail.com>
2013-09-17 14:08:16 +0200

    Fix dla przypadku, gdy pytanie o danym id jest, ale nie ma quizu o podanym id.

--- quiz_management/app/helpers/quiz_helper.rb
+++ quiz_management/app/helpers/quiz_helper.rb
@@ -2,8 +2,8 @@
 module QuizHelper
 	def quiz_available?
 		begin
-			quiz = Quiz.find(params[:id_quizu])
 			grupa_quizowa = GrupaQuizowa.find(params[:id_grupy])
+			quiz = Quiz.find(params[:id_quizu])
 			unless grupa_quizowa.id_grupy == quiz.id_grupy
 				redirect_to grupa_url(:id_grupy => grupa_quizowa.id_grupy), :alert => "Quiz nie jest dostępny w grupe."
 			end
