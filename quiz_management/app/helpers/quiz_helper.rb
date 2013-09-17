96f97e034f0af8a68c00fda0c5be7887b7aa41f5
jam231 <mateusz.jany@gmail.com>
2013-09-17 21:14:17 +0200

    Issue #59. Wydaje mi sie, ze poprawilem to co bylo nie tak z ukrytymi pytaniami i dodalem ukrywanie\udostepnianie quizu.

--- quiz_management/app/helpers/quiz_helper.rb
+++ quiz_management/app/helpers/quiz_helper.rb
@@ -4,11 +4,11 @@ module QuizHelper
 		begin
 			grupa_quizowa = GrupaQuizowa.find(params[:id_grupy])
 			quiz = Quiz.find(params[:id_quizu])
-			unless grupa_quizowa.id_grupy == quiz.id_grupy
-				redirect_to grupa_url(:id_grupy => grupa_quizowa.id_grupy), :alert => "Quiz nie jest dostępny w grupe."
+			unless grupa_quizowa.id_grupy == quiz.id_grupy and not quiz.ukryty
+				redirect_to grupa_url(grupa_quizowa), :alert => "Quiz nie jest dostępny w grupe."
 			end
 		rescue ActiveRecord::RecordNotFound
-			redirect_to grupa_url(:id_grupy => grupa_quizowa.id_grupy), :alert => "Quiz nie jest dostępny."
+			redirect_to grupa_url(grupa_quizowa), :alert => "Quiz nie jest dostępny."
 		end
 	end
 end
