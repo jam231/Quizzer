0b00781f0ddff292231cf32a0b3eb97cd8868271
jam231 <mateusz.jany@gmail.com>
2013-09-17 21:16:10 +0200

    Issue #59. Ehm, literowka.

--- quiz_management/app/helpers/quiz_helper.rb
+++ quiz_management/app/helpers/quiz_helper.rb
@@ -5,7 +5,7 @@ module QuizHelper
 			grupa_quizowa = GrupaQuizowa.find(params[:id_grupy])
 			quiz = Quiz.find(params[:id_quizu])
 			unless grupa_quizowa.id_grupy == quiz.id_grupy and not quiz.ukryty
-				redirect_to grupa_url(grupa_quizowa), :alert => "Quiz nie jest dostępny w grupe."
+				redirect_to grupa_url(grupa_quizowa), :alert => "Quiz nie jest dostępny w grupie."
 			end
 		rescue ActiveRecord::RecordNotFound
 			redirect_to grupa_url(grupa_quizowa), :alert => "Quiz nie jest dostępny."
