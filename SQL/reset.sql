ec8b1b9f2f79c24b4ad9e4c49627c41eb679e63f
jam231 <mateusz.jany@gmail.com>
2013-09-18 18:15:13 +0200

    Issue #71. Zmodyfikowano schemat tabel uzytkownik i grupa_quizowa, dodano bcrypta i hashowanie hasel. Przy formularzu rejestracji uzytkownika zostal dodany input dla powtorzenia hasla i poprawnie dziala sprawdzanie.

--- SQL/reset.sql
+++ SQL/reset.sql
@@ -5,5 +5,4 @@ CREATE SCHEMA public;
 \i 'model fizyczny.sql'
 \i 'role.sql'
 \i 'dane.sql'
-\i 'przykladowi_uzytkownicy.sql'
-\i 'losowe_quizy.sql'
+--\i 'losowe_quizy.sql'
--- quiz_management/app/models/uzytkownik.rb
+++ quiz_management/app/models/uzytkownik.rb
@@ -1,7 +1,9 @@
 # encoding: UTF-8
-class Uzytkownik < ActiveRecord::Base
 
-  self.table_name = 'uzytkownik'
+class Uzytkownik < ActiveRecord::Base
+	include BCrypt
+
+	self.table_name = 'uzytkownik'
   self.primary_key = :id_uz
 
   has_many :quizy, :foreign_key => 'id_wlasciciela', :class_name => "Quiz"
@@ -14,33 +16,49 @@ class Uzytkownik < ActiveRecord::Base
             :format => {:with => /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/}
   validates :nazwa_uz, :presence => true, :uniqueness => true, :length => {:maximum => 30}
   validates :login, :presence => true, :uniqueness => true, :length => {:maximum => 15}
-  validates :haslo, :presence => true, :length => {:in => 1..30}
-
+  validates :password, :presence => true, :length => {:in => 1..30}, :on => :create
+  validates_confirmation_of :password
 
   # Po co ?
   # Odsylam do http://blog.remarkablelabs.com/2012/12/strong-parameters-rails-4-countdown-to-2013
-  attr_accessible :nazwa_uz, :login, :haslo, :email, :password, :ranga
+  attr_accessible :nazwa_uz, :login, :password, :email, :password_confirmation, :ranga
   attr_accessor :password
+  before_save :encrypt_password
 
-  def limbo?
-		self.id_uz == 0
-  end
 
-  def self.Limbo
-		Uzytkownk.find(0)
-  end
-
-  def can_create_new_groups?
-	  self.superuser? or self.teacher?
+  def encrypt_password
+	  logger.debug "Hashuje haslo #{password}."
+	  if password.present?
+		  self.haslo_salt = BCrypt::Engine.generate_salt
+		  logger.debug "Wygenerowany salt = #{haslo_salt}."
+		  self.haslo = BCrypt::Engine.hash_secret(password, haslo_salt)
+		  logger.debug "Hash = #{self.haslo}."
+	  end
   end
 
   def self.authenticate(login, password)
     user = find_by_login(login)
-    if user && user.haslo == password
+    logger.debug "Użytkownik o loginie #{login} stara sie zalogowac"
+    logger.debug "Haslo to #{password}, salt to #{user.haslo_salt}."
+    logger.debug "#{BCrypt::Engine.hash_secret(password, user.haslo_salt)}"
+    if user && user.haslo == BCrypt::Engine.hash_secret(password, user.haslo_salt)
       user
     else
       nil
     end
+  end
+
+
+  def limbo?
+	  self.id_uz == 0
+  end
+
+  def self.Limbo
+	  Uzytkownk.find(0)
+  end
+
+  def can_create_new_groups?
+	  self.superuser? or self.teacher?
   end
 
   def normal_user?
@@ -53,6 +71,14 @@ class Uzytkownik < ActiveRecord::Base
 	  is_teacher = (self.ranga == 'teacher')
 	  logger.debug "Czy użytkownik #{self.nazwa_uz} jest nauczycielem ? : #{is_teacher} "
 	  is_teacher
+  end
+
+  def moderator?
+		self.ranga == 'moderator'
+  end
+
+  def administrator?
+		self.administrator == 'administrator'
   end
 
   def superuser?
